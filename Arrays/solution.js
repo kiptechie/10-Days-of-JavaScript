ae983fadf955842a8d465d963424fcc5094d2795
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-26 14:18:08 +0300

    Create solution.js

---
+++ Arrays/solution.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+function getSecondLargest(nums) {
+    // Complete the function
+    const max = Math.max(...nums);
+
+    nums = nums.filter(num => num !== max);
+
+    return Math.max(...nums);
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+
+    console.log(getSecondLargest(nums));
+}
